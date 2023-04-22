72d026d2e9a57d1c6f19e47f6bae97ff0435032f
rekla <filatovprivate@gmail.com>
2023-04-23 01:06:04 +0800

    feat: add hand values as enum

---
+++ src/main/java/org/filatov/pokerhandranking/rules/HandValue.java
@@ -0,0 +1,14 @@
+package org.filatov.pokerhandranking.rules;
+
+public enum HandValue {
+    HIGH_CARD,
+    PAIR,
+    TWO_PAIRS,
+    THREE_OF_A_KIND,
+    STRAIGHT,
+    FLUSH,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    STRAIGHT_FLUSH,
+    ROYAL_FLUSH
+}
