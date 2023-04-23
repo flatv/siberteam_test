cd58fec5f7840142b53dbc9cb7c432c4089fcbd6
rekla <filatovprivate@gmail.com>
2023-04-23 18:46:18 +0800

    test: add correct test

--- src/test/java/org/filatov/pokerhandranking/HandValueEvaluatorTest.java
+++ src/test/java/org/filatov/pokerhandranking/HandValueEvaluatorTest.java
@@ -11,32 +11,31 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 import java.util.stream.Stream;
 
 import static org.filatov.pokerhandranking.HandValueEvaluator.evaluate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HandValueEvaluatorTest {
 
-
     @ParameterizedTest
     @ArgumentsSource(PokerHandsArgumentProvider.class)
-    void evaluateTest(PokerHand hand) {
+    void evaluateTest(PokerHand hand, HandValue handValue) {
         HandValue value = evaluate(hand);
-
-        System.out.println(value);
+        assertEquals(handValue, value);
     }
 
     static class PokerHandsArgumentProvider implements ArgumentsProvider {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
             return Stream.of(
-                    Arguments.of(new PokerHand("TC 4H 7D KC 2S")),
-                    Arguments.of(new PokerHand("2C 3C AC 4C AD")),
-                    Arguments.of(new PokerHand("KS KH TC JD TD")),
-                    Arguments.of(new PokerHand("KC KH KD 7C 5S")),
-                    Arguments.of(new PokerHand("3C 4H 5D 6C 7S")),
-                    Arguments.of(new PokerHand("KC QC 9C 8C 2C")),
-                    Arguments.of(new PokerHand("KC KH KD 7C 7D")),
-                    Arguments.of(new PokerHand("6S 6H 6D 6C KD")),
-                    Arguments.of(new PokerHand("2D 3D 4D 5D 6D")),
-                    Arguments.of(new PokerHand("AH KH QH JH TH"))
+                    Arguments.of(new PokerHand("TC 4H 7D KC 2S"), HandValue.HIGH_CARD),
+                    Arguments.of(new PokerHand("2C 3C AC 4C AD"), HandValue.PAIR),
+                    Arguments.of(new PokerHand("KS KH TC JD TD"), HandValue.TWO_PAIRS),
+                    Arguments.of(new PokerHand("KC KH KD 7C 5S"), HandValue.THREE_OF_A_KIND),
+                    Arguments.of(new PokerHand("3C 4H 5D 6C 7S"), HandValue.STRAIGHT),
+                    Arguments.of(new PokerHand("KC QC 9C 8C 2C"), HandValue.FLUSH),
+                    Arguments.of(new PokerHand("KC KH KD 7C 7D"), HandValue.FULL_HOUSE),
+                    Arguments.of(new PokerHand("6S 6H 6D 6C KD"), HandValue.FOUR_OF_A_KIND),
+                    Arguments.of(new PokerHand("2D 3D 4D 5D 6D"), HandValue.STRAIGHT_FLUSH),
+                    Arguments.of(new PokerHand("AH KH QH JH TH"), HandValue.ROYAL_FLUSH)
             );
         }
     }
