d084b084845914cdf0b65b83817d56219ce4f12e
rekla <filatovprivate@gmail.com>
2023-04-24 01:37:43 +0800

    chore: refactor extract method

--- src/main/java/org/filatov/pokerhandranking/util/HandValueEvaluator.java
+++ src/main/java/org/filatov/pokerhandranking/util/HandValueEvaluator.java
@@ -1,10 +1,11 @@
-package org.filatov.pokerhandranking;
+package org.filatov.pokerhandranking.util;
 
 import org.filatov.pokerhandranking.game.Card;
 import org.filatov.pokerhandranking.game.PokerHand;
 import org.filatov.pokerhandranking.rules.HandValue;
 import org.filatov.pokerhandranking.rules.Rank;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 
@@ -27,10 +28,9 @@ public class HandValueEvaluator {
         if (count == 2) {
             return cardsCount[0] == 3 ? HandValue.FULL_HOUSE : HandValue.FOUR_OF_A_KIND;
         }
-        if (ranks[0].ordinal() - ranks[4].ordinal() == 4) {
+        if (isStraight(ranks)) {
             return flush == 1 ? HandValue.STRAIGHT_FLUSH : HandValue.STRAIGHT;
         }
-        //TODO: флеш рояль не работает
         if (ranks[0].equals(Rank.ACE) && ranks[1].equals(Rank.KING) && flush == 1) {
             return HandValue.ROYAL_FLUSH;
         }
@@ -38,17 +38,19 @@ public class HandValueEvaluator {
         return flush == 1 ? HandValue.FLUSH : HandValue.HIGH_CARD;
     }
 
+    private static boolean isStraight(Rank[] ranks) {
+        return ranks[0].ordinal() - ranks[4].ordinal() == 4 && ranks[0] != Rank.ACE;
+    }
+
     private static Rank[] getRanks(PokerHand hand) {
-        return hand.getHand()
-                .stream()
+        return Arrays.stream(hand.getHand())
                 .map(Card::rank)
                 .sorted(Comparator.reverseOrder())
                 .toArray(Rank[]::new);
     }
 
     private static Integer[] getCardsCount(PokerHand hand) {
-        return hand.getHand()
-                .stream()
+        return Arrays.stream(hand.getHand())
                 .map(Card::rank)
                 .collect(Collectors.toMap(
                         card -> card,
@@ -62,16 +64,14 @@ public class HandValueEvaluator {
     }
 
     private static long getFlush(PokerHand hand) {
-        return hand.getHand()
-                .stream()
+        return Arrays.stream(hand.getHand())
                 .map(Card::suit)
                 .distinct()
                 .count();
     }
 
     private static long getCount(PokerHand hand) {
-        return hand.getHand()
-                .stream()
+        return Arrays.stream(hand.getHand())
                 .map(Card::rank)
                 .distinct()
                 .count();
